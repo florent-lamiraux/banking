a9a829af00c26c08f029c54b4fd5f628d423d949
florent <florent@laas.fr>
2010-07-13 21:02:56 +0200

    Implement line and import in frame
    	  * frame.py,
    	  * line.py.

--- frame.py
+++ frame.py
@@ -2,19 +2,10 @@
 
 import wx
 import account
+from line import *
 
 def create(parent):
     return Frame(parent)
-
-[wxID_FRAME, wxID_FRAMEBANKCHECKBOX1, wxID_FRAMEBANKSTATICTEXT, 
- wxID_FRAMEDATESTATICTEXT, wxID_FRAMEDATETEXTCTRL1, 
- wxID_FRAMELIBELLESTATICTEXT, wxID_FRAMELIBELLETEXTCTRL1, 
- wxID_FRAMEMODESTATICTEXT, wxID_FRAMEMODETEXTCTRL1, 
- wxID_FRAMEMONTANTSTATICTEXT, wxID_FRAMEMONTANTTEXTCTRL1, 
- wxID_FRAMESCROLLEDWINDOW, wxID_FRAMESELECTCHECKBOX1, 
- wxID_FRAMETOTALBANKTEXTCTRL, wxID_FRAMETOTALBANQUESTATICTEXT, 
- wxID_FRAMETOTALSTATICTEXT, wxID_FRAMETOTALTEXTCTRL, 
-] = [wx.NewId() for _init_ctrls in range(17)]
 
 [wxID_FRAMEFILEOPEN, wxID_FRAMEFILEQUIT, wxID_FRAMEFILESAVE, 
 ] = [wx.NewId() for _init_coll_File_Items in range(3)]
@@ -99,70 +90,7 @@ class Frame(wx.Frame):
               parent=self.scrolledWindow, pos=wx.Point(230, 8),
               size=wx.Size(800, 20), style=wx.ALIGN_CENTRE)
 
-        self.dateTextCtrl1 = wx.TextCtrl(id=wxID_FRAMEDATETEXTCTRL1,
-              name=u'dateTextCtrl1', parent=self.scrolledWindow, pos=wx.Point(0,
-              32), size=wx.Size(99, 19), style=0, value=u'25/11/2010')
-        self.dateTextCtrl1.Bind(wx.EVT_TEXT_MAXLEN,
-              self.OnDateTextCtrl1TextMaxlen, id=wxID_FRAMEDATETEXTCTRL1)
-        self.dateTextCtrl1.Bind(wx.EVT_TEXT_URL, self.OnDateTextCtrl1TextUrl,
-              id=wxID_FRAMEDATETEXTCTRL1)
-        self.dateTextCtrl1.Bind(wx.EVT_TEXT_ENTER,
-              self.OnDateTextCtrl1TextEnter, id=wxID_FRAMEDATETEXTCTRL1)
-        self.dateTextCtrl1.Bind(wx.EVT_TEXT, self.OnDateTextCtrl1Text,
-              id=wxID_FRAMEDATETEXTCTRL1)
-
-        self.modeTextCtrl1 = wx.TextCtrl(id=wxID_FRAMEMODETEXTCTRL1,
-              name=u'modeTextCtrl1', parent=self.scrolledWindow,
-              pos=wx.Point(100, 32), size=wx.Size(129, 19), style=0,
-              value=u'CB Aur\xe9lie')
-        self.modeTextCtrl1.Bind(wx.EVT_TEXT_MAXLEN,
-              self.OnModeTextCtrl1TextMaxlen, id=wxID_FRAMEMODETEXTCTRL1)
-        self.modeTextCtrl1.Bind(wx.EVT_TEXT_URL, self.OnModeTextCtrl1TextUrl,
-              id=wxID_FRAMEMODETEXTCTRL1)
-        self.modeTextCtrl1.Bind(wx.EVT_TEXT_ENTER,
-              self.OnModeTextCtrl1TextEnter, id=wxID_FRAMEMODETEXTCTRL1)
-        self.modeTextCtrl1.Bind(wx.EVT_TEXT, self.OnModeTextCtrl1Text,
-              id=wxID_FRAMEMODETEXTCTRL1)
-
-        self.libelleTextCtrl1 = wx.TextCtrl(id=wxID_FRAMELIBELLETEXTCTRL1,
-              name=u'libelleTextCtrl1', parent=self.scrolledWindow,
-              pos=wx.Point(230, 32), size=wx.Size(799, 19), style=0,
-              value=u'Chronodrive.com')
-        self.libelleTextCtrl1.Bind(wx.EVT_TEXT_URL,
-              self.OnLibelleTextCtrl1TextUrl, id=wxID_FRAMELIBELLETEXTCTRL1)
-        self.libelleTextCtrl1.Bind(wx.EVT_TEXT_MAXLEN,
-              self.OnLibelleTextCtrl1TextMaxlen, id=wxID_FRAMELIBELLETEXTCTRL1)
-        self.libelleTextCtrl1.Bind(wx.EVT_TEXT_ENTER,
-              self.OnLibelleTextCtrl1TextEnter, id=wxID_FRAMELIBELLETEXTCTRL1)
-        self.libelleTextCtrl1.Bind(wx.EVT_TEXT, self.OnLibelleTextCtrl1Text,
-              id=wxID_FRAMELIBELLETEXTCTRL1)
-
-        self.bankCheckBox1 = wx.CheckBox(id=wxID_FRAMEBANKCHECKBOX1, label=u'',
-              name=u'bankCheckBox1', parent=self.scrolledWindow,
-              pos=wx.Point(1048, 32), size=wx.Size(19, 19), style=0)
-        self.bankCheckBox1.SetValue(False)
-        self.bankCheckBox1.Bind(wx.EVT_CHECKBOX, self.OnBankCheckBox1Checkbox,
-              id=wxID_FRAMEBANKCHECKBOX1)
-        self.bankCheckBox1.Bind(wx.EVT_HELP, self.OnBankCheckBox1Help,
-              id=wxID_FRAMEBANKCHECKBOX1)
-
-        self.montantTextCtrl1 = wx.TextCtrl(id=wxID_FRAMEMONTANTTEXTCTRL1,
-              name=u'montantTextCtrl1', parent=self.scrolledWindow,
-              pos=wx.Point(1100, 32), size=wx.Size(149, 19), style=wx.TE_RIGHT,
-              value=u'-83.56')
-        self.montantTextCtrl1.Bind(wx.EVT_TEXT_MAXLEN,
-              self.OnMontantTextCtrl1TextMaxlen, id=wxID_FRAMEMONTANTTEXTCTRL1)
-        self.montantTextCtrl1.Bind(wx.EVT_TEXT_URL,
-              self.OnMontantTextCtrl1TextUrl, id=wxID_FRAMEMONTANTTEXTCTRL1)
-        self.montantTextCtrl1.Bind(wx.EVT_TEXT_ENTER,
-              self.OnMontantTextCtrl1TextEnter, id=wxID_FRAMEMONTANTTEXTCTRL1)
-        self.montantTextCtrl1.Bind(wx.EVT_TEXT, self.OnMontantTextCtrl1Text,
-              id=wxID_FRAMEMONTANTTEXTCTRL1)
-
-        self.selectCheckBox1 = wx.CheckBox(id=wxID_FRAMESELECTCHECKBOX1,
-              label=u'', name=u'selectCheckBox1', parent=self.scrolledWindow,
-              pos=wx.Point(1264, 32), size=wx.Size(19, 19), style=0)
-        self.selectCheckBox1.SetValue(False)
+        self.line = Line()
 
         self.totalBanqueStaticText = wx.StaticText(id=wxID_FRAMETOTALBANQUESTATICTEXT,
               label=u'Total banque', name=u'totalBanqueStaticText',
@@ -233,56 +161,3 @@ class Frame(wx.Frame):
         self.SetTitle(("Pas de fichier"))
         event.Skip()
 
-    def OnBankCheckBox1Checkbox(self, event):
-        event.Skip()
-
-    def OnBankCheckBox1Help(self, event):
-        event.Skip()
-
-    def OnDateTextCtrl1TextMaxlen(self, event):
-        event.Skip()
-
-    def OnDateTextCtrl1TextUrl(self, event):
-        event.Skip()
-
-    def OnDateTextCtrl1TextEnter(self, event):
-        event.Skip()
-
-    def OnDateTextCtrl1Text(self, event):
-        event.Skip()
-
-    def OnModeTextCtrl1TextMaxlen(self, event):
-        event.Skip()
-
-    def OnModeTextCtrl1TextUrl(self, event):
-        event.Skip()
-
-    def OnModeTextCtrl1TextEnter(self, event):
-        event.Skip()
-
-    def OnModeTextCtrl1Text(self, event):
-        event.Skip()
-
-    def OnLibelleTextCtrl1TextUrl(self, event):
-        event.Skip()
-
-    def OnLibelleTextCtrl1TextMaxlen(self, event):
-        event.Skip()
-
-    def OnLibelleTextCtrl1TextEnter(self, event):
-        event.Skip()
-
-    def OnLibelleTextCtrl1Text(self, event):
-        event.Skip()
-
-    def OnMontantTextCtrl1TextMaxlen(self, event):
-        event.Skip()
-
-    def OnMontantTextCtrl1TextUrl(self, event):
-        event.Skip()
-
-    def OnMontantTextCtrl1TextEnter(self, event):
-        event.Skip()
-
-    def OnMontantTextCtrl1Text(self, event):
-        event.Skip()
--- line.py
+++ line.py
@@ -1,9 +1,5 @@
-#Boa:Frame:Frame
-
 import wx
-
-def create(parent):
-    return Frame(parent)
+import Entry
 
 [wxID_FRAME, wxID_FRAMEBANKCHECKBOX1, wxID_FRAMEBANKSTATICTEXT, 
  wxID_FRAMEDATESTATICTEXT, wxID_FRAMEDATETEXTCTRL1, 
@@ -15,141 +11,126 @@ def create(parent):
  wxID_FRAMETOTALSTATICTEXT, wxID_FRAMETOTALTEXTCTRL, 
 ] = [wx.NewId() for _init_ctrls in range(17)]
 
-[wxID_FRAMEFILEOPEN, wxID_FRAMEFILEQUIT, wxID_FRAMEFILESAVE, 
-] = [wx.NewId() for _init_coll_File_Items in range(3)]
-
-[wxID_FRAMEMENUFILEOPEN, wxID_FRAMEMENUFILEQUIT, wxID_FRAMEMENUFILESAVE, 
-] = [wx.NewId() for _init_coll_menuFile_Items in range(3)]
-
-class Frame(wx.Frame):
-    def _init_coll_menuBar1_Menus(self, parent):
-        # generated method, don't edit
-
-        parent.Append(menu=self.menuFile, title=u'Fichier')
-
-    def _init_coll_menuFile_Items(self, parent):
-        # generated method, don't edit
-
-        parent.Append(help=u'Ouvrir un fichier de comptes',
-              id=wxID_FRAMEMENUFILEOPEN, kind=wx.ITEM_NORMAL, text=u'Ouvrir')
-        parent.Append(help=u'Enregistrer les ecritures dans un fichier',
-              id=wxID_FRAMEMENUFILESAVE, kind=wx.ITEM_NORMAL,
-              text=u'Enregistrer')
-        parent.Append(help=u"Quitter l'application", id=wxID_FRAMEMENUFILEQUIT,
-              kind=wx.ITEM_NORMAL, text=u'Quitter')
-        self.Bind(wx.EVT_MENU, self.OnFileOpenMenu, id=wxID_FRAMEMENUFILEOPEN)
-        self.Bind(wx.EVT_MENU, self.OnFileSaveMenu, id=wxID_FRAMEMENUFILESAVE)
-        self.Bind(wx.EVT_MENU, self.OnFileQuitMenu, id=wxID_FRAMEMENUFILEQUIT)
-
-    def _init_utils(self):
-        # generated method, don't edit
-        self.menuFile = wx.Menu(title=u'Fichier')
-
-        self.menuBar1 = wx.MenuBar()
-
-        self._init_coll_menuFile_Items(self.menuFile)
-        self._init_coll_menuBar1_Menus(self.menuBar1)
-
-    def _init_ctrls(self, prnt):
-        # generated method, don't edit
-        wx.Frame.__init__(self, id=wxID_FRAME, name='', parent=prnt,
-              pos=wx.Point(0, 25), size=wx.Size(1910, 1114),
-              style=wx.DEFAULT_FRAME_STYLE, title=u'Comptes')
-        self._init_utils()
-        self.SetClientSize(wx.Size(1910, 1114))
-        self.SetMenuBar(self.menuBar1)
-
-        self.scrolledWindow = wx.ScrolledWindow(id=wxID_FRAMESCROLLEDWINDOW,
-              name=u'scrolledWindow', parent=self, pos=wx.Point(0, 0),
-              size=wx.Size(1910, 1114), style=wx.HSCROLL | wx.VSCROLL)
-
-        self.dateStaticText = wx.StaticText(id=wxID_FRAMEDATESTATICTEXT,
-              label=u'Date', name=u'dateStaticText', parent=self.scrolledWindow,
-              pos=wx.Point(0, 38), size=wx.Size(100, 20),
-              style=wx.ALIGN_CENTRE)
-        self.dateStaticText.SetBackgroundColour(wx.Colour(4, 0, 0))
-
-        self.modeStaticText = wx.StaticText(id=wxID_FRAMEMODESTATICTEXT,
-              label=u'Mode de Paiement', name=u'modeStaticText',
-              parent=self.scrolledWindow, pos=wx.Point(100, 38),
-              size=wx.Size(130, 20), style=wx.ALIGN_CENTRE)
-
-        self.montantStaticText = wx.StaticText(id=wxID_FRAMEMONTANTSTATICTEXT,
-              label=u'Montant', name=u'montantStaticText',
-              parent=self.scrolledWindow, pos=wx.Point(1100, 38),
-              size=wx.Size(150, 20), style=wx.ALIGN_CENTRE)
-
-        self.bankStaticText = wx.StaticText(id=wxID_FRAMEBANKSTATICTEXT,
-              label=u'Banque', name=u'bankStaticText',
-              parent=self.scrolledWindow, pos=wx.Point(1030, 38),
-              size=wx.Size(70, 20), style=wx.ALIGN_CENTRE)
-        self.bankStaticText.SetHelpText(u'Cette entr\xe9e est-elle prise en compte par la banque ?')
-
-        self.libelleStaticText = wx.StaticText(id=wxID_FRAMELIBELLESTATICTEXT,
-              label=u'Libell\xe9', name=u'libelleStaticText',
-              parent=self.scrolledWindow, pos=wx.Point(230, 38),
-              size=wx.Size(800, 20), style=wx.ALIGN_CENTRE)
-
+class Line (object) :
+    """
+    One line in the account book
+    """
+    def __init__(self, entry) :
         self.dateTextCtrl1 = wx.TextCtrl(id=wxID_FRAMEDATETEXTCTRL1,
               name=u'dateTextCtrl1', parent=self.scrolledWindow, pos=wx.Point(0,
-              62), size=wx.Size(99, 19), style=0, value=u'25/11/2010')
+              32), size=wx.Size(99, 19), style=0, value=u'25/11/2010')
+        self.dateTextCtrl1.Bind(wx.EVT_TEXT_MAXLEN,
+              self.OnDateTextCtrl1TextMaxlen, id=wxID_FRAMEDATETEXTCTRL1)
+        self.dateTextCtrl1.Bind(wx.EVT_TEXT_URL, self.OnDateTextCtrl1TextUrl,
+              id=wxID_FRAMEDATETEXTCTRL1)
+        self.dateTextCtrl1.Bind(wx.EVT_TEXT_ENTER,
+              self.OnDateTextCtrl1TextEnter, id=wxID_FRAMEDATETEXTCTRL1)
+        self.dateTextCtrl1.Bind(wx.EVT_TEXT, self.OnDateTextCtrl1Text,
+              id=wxID_FRAMEDATETEXTCTRL1)
 
         self.modeTextCtrl1 = wx.TextCtrl(id=wxID_FRAMEMODETEXTCTRL1,
               name=u'modeTextCtrl1', parent=self.scrolledWindow,
-              pos=wx.Point(100, 62), size=wx.Size(129, 19), style=0,
+              pos=wx.Point(100, 32), size=wx.Size(129, 19), style=0,
               value=u'CB Aur\xe9lie')
+        self.modeTextCtrl1.Bind(wx.EVT_TEXT_MAXLEN,
+              self.OnModeTextCtrl1TextMaxlen, id=wxID_FRAMEMODETEXTCTRL1)
+        self.modeTextCtrl1.Bind(wx.EVT_TEXT_URL, self.OnModeTextCtrl1TextUrl,
+              id=wxID_FRAMEMODETEXTCTRL1)
+        self.modeTextCtrl1.Bind(wx.EVT_TEXT_ENTER,
+              self.OnModeTextCtrl1TextEnter, id=wxID_FRAMEMODETEXTCTRL1)
+        self.modeTextCtrl1.Bind(wx.EVT_TEXT, self.OnModeTextCtrl1Text,
+              id=wxID_FRAMEMODETEXTCTRL1)
 
         self.libelleTextCtrl1 = wx.TextCtrl(id=wxID_FRAMELIBELLETEXTCTRL1,
               name=u'libelleTextCtrl1', parent=self.scrolledWindow,
-              pos=wx.Point(230, 62), size=wx.Size(799, 19), style=0,
+              pos=wx.Point(230, 32), size=wx.Size(799, 19), style=0,
               value=u'Chronodrive.com')
+        self.libelleTextCtrl1.Bind(wx.EVT_TEXT_URL,
+              self.OnLibelleTextCtrl1TextUrl, id=wxID_FRAMELIBELLETEXTCTRL1)
+        self.libelleTextCtrl1.Bind(wx.EVT_TEXT_MAXLEN,
+              self.OnLibelleTextCtrl1TextMaxlen, id=wxID_FRAMELIBELLETEXTCTRL1)
+        self.libelleTextCtrl1.Bind(wx.EVT_TEXT_ENTER,
+              self.OnLibelleTextCtrl1TextEnter, id=wxID_FRAMELIBELLETEXTCTRL1)
+        self.libelleTextCtrl1.Bind(wx.EVT_TEXT, self.OnLibelleTextCtrl1Text,
+              id=wxID_FRAMELIBELLETEXTCTRL1)
 
         self.bankCheckBox1 = wx.CheckBox(id=wxID_FRAMEBANKCHECKBOX1, label=u'',
               name=u'bankCheckBox1', parent=self.scrolledWindow,
-              pos=wx.Point(1048, 62), size=wx.Size(19, 19), style=0)
+              pos=wx.Point(1048, 32), size=wx.Size(19, 19), style=0)
         self.bankCheckBox1.SetValue(False)
+        self.bankCheckBox1.Bind(wx.EVT_CHECKBOX, self.OnBankCheckBox1Checkbox,
+              id=wxID_FRAMEBANKCHECKBOX1)
+        self.bankCheckBox1.Bind(wx.EVT_HELP, self.OnBankCheckBox1Help,
+              id=wxID_FRAMEBANKCHECKBOX1)
 
         self.montantTextCtrl1 = wx.TextCtrl(id=wxID_FRAMEMONTANTTEXTCTRL1,
               name=u'montantTextCtrl1', parent=self.scrolledWindow,
-              pos=wx.Point(1100, 62), size=wx.Size(149, 19), style=wx.TE_RIGHT,
+              pos=wx.Point(1100, 32), size=wx.Size(149, 19), style=wx.TE_RIGHT,
               value=u'-83.56')
+        self.montantTextCtrl1.Bind(wx.EVT_TEXT_MAXLEN,
+              self.OnMontantTextCtrl1TextMaxlen, id=wxID_FRAMEMONTANTTEXTCTRL1)
+        self.montantTextCtrl1.Bind(wx.EVT_TEXT_URL,
+              self.OnMontantTextCtrl1TextUrl, id=wxID_FRAMEMONTANTTEXTCTRL1)
+        self.montantTextCtrl1.Bind(wx.EVT_TEXT_ENTER,
+              self.OnMontantTextCtrl1TextEnter, id=wxID_FRAMEMONTANTTEXTCTRL1)
+        self.montantTextCtrl1.Bind(wx.EVT_TEXT, self.OnMontantTextCtrl1Text,
+              id=wxID_FRAMEMONTANTTEXTCTRL1)
 
         self.selectCheckBox1 = wx.CheckBox(id=wxID_FRAMESELECTCHECKBOX1,
               label=u'', name=u'selectCheckBox1', parent=self.scrolledWindow,
-              pos=wx.Point(1264, 62), size=wx.Size(19, 19), style=0)
+              pos=wx.Point(1264, 32), size=wx.Size(19, 19), style=0)
         self.selectCheckBox1.SetValue(False)
 
-        self.totalBanqueStaticText = wx.StaticText(id=wxID_FRAMETOTALBANQUESTATICTEXT,
-              label=u'Total banque', name=u'totalBanqueStaticText',
-              parent=self.scrolledWindow, pos=wx.Point(900, 112),
-              size=wx.Size(129, 19), style=0)
-
-        self.totalStaticText = wx.StaticText(id=wxID_FRAMETOTALSTATICTEXT,
-              label=u'Total', name=u'totalStaticText',
-              parent=self.scrolledWindow, pos=wx.Point(900, 92),
-              size=wx.Size(129, 20), style=0)
-
-        self.totalTextCtrl = wx.TextCtrl(id=wxID_FRAMETOTALTEXTCTRL,
-              name=u'totalTextCtrl', parent=self.scrolledWindow,
-              pos=wx.Point(1100, 92), size=wx.Size(149, 19), style=wx.TE_RIGHT,
-              value=u'1000.05')
-
-        self.totalBankTextCtrl = wx.TextCtrl(id=wxID_FRAMETOTALBANKTEXTCTRL,
-              name=u'totalBankTextCtrl', parent=self.scrolledWindow,
-              pos=wx.Point(1100, 112), size=wx.Size(149, 19), style=wx.TE_RIGHT,
-              value=u'982.14')
-
-    def __init__(self, parent):
-        self._init_ctrls(parent)
-
-    def OnFileItems0Menu(self, event):
+    def OnBankCheckBox1Checkbox(self, event):
         event.Skip()
 
-    def OnFileOpenMenu(self, event):
+    def OnBankCheckBox1Help(self, event):
         event.Skip()
 
-    def OnFileSaveMenu(self, event):
+    def OnDateTextCtrl1TextMaxlen(self, event):
         event.Skip()
 
-    def OnFileQuitMenu(self, event):
+    def OnDateTextCtrl1TextUrl(self, event):
+        event.Skip()
+
+    def OnDateTextCtrl1TextEnter(self, event):
+        event.Skip()
+
+    def OnDateTextCtrl1Text(self, event):
+        event.Skip()
+
+    def OnModeTextCtrl1TextMaxlen(self, event):
+        event.Skip()
+
+    def OnModeTextCtrl1TextUrl(self, event):
+        event.Skip()
+
+    def OnModeTextCtrl1TextEnter(self, event):
+        event.Skip()
+
+    def OnModeTextCtrl1Text(self, event):
+        event.Skip()
+
+    def OnLibelleTextCtrl1TextUrl(self, event):
+        event.Skip()
+
+    def OnLibelleTextCtrl1TextMaxlen(self, event):
+        event.Skip()
+
+    def OnLibelleTextCtrl1TextEnter(self, event):
+        event.Skip()
+
+    def OnLibelleTextCtrl1Text(self, event):
+        event.Skip()
+
+    def OnMontantTextCtrl1TextMaxlen(self, event):
+        event.Skip()
+
+    def OnMontantTextCtrl1TextUrl(self, event):
+        event.Skip()
+
+    def OnMontantTextCtrl1TextEnter(self, event):
+        event.Skip()
+
+    def OnMontantTextCtrl1Text(self, event):
         event.Skip()
