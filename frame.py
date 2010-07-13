3c2f67f0e7450a8fac54fb2fc743e855d823d29a
florent <florent@laas.fr>
2010-07-13 21:25:32 +0200

    Move lines up
         * frame.py,
         * frame.pyc: deleted.

--- frame.py
+++ frame.py
@@ -61,82 +61,81 @@ class Frame(wx.Frame):
 
         self.scrolledWindow = wx.ScrolledWindow(id=wxID_FRAMESCROLLEDWINDOW,
               name=u'scrolledWindow', parent=self, pos=wx.Point(0, 0),
-              size=wx.Size(1910, 1114), style=wx.HSCROLL | wx.VSCROLL)
+              size=wx.Size(1910, 1085), style=wx.HSCROLL | wx.VSCROLL)
 
         self.dateStaticText = wx.StaticText(id=wxID_FRAMEDATESTATICTEXT,
               label=u'Date', name=u'dateStaticText', parent=self.scrolledWindow,
-              pos=wx.Point(0, 38), size=wx.Size(100, 20),
-              style=wx.ALIGN_CENTRE)
+              pos=wx.Point(0, 8), size=wx.Size(100, 20), style=wx.ALIGN_CENTRE)
         self.dateStaticText.SetBackgroundColour(wx.Colour(4, 0, 0))
 
         self.modeStaticText = wx.StaticText(id=wxID_FRAMEMODESTATICTEXT,
               label=u'Mode de Paiement', name=u'modeStaticText',
-              parent=self.scrolledWindow, pos=wx.Point(100, 38),
+              parent=self.scrolledWindow, pos=wx.Point(100, 8),
               size=wx.Size(130, 20), style=wx.ALIGN_CENTRE)
 
         self.montantStaticText = wx.StaticText(id=wxID_FRAMEMONTANTSTATICTEXT,
               label=u'Montant', name=u'montantStaticText',
-              parent=self.scrolledWindow, pos=wx.Point(1100, 38),
+              parent=self.scrolledWindow, pos=wx.Point(1100, 8),
               size=wx.Size(150, 20), style=wx.ALIGN_CENTRE)
 
         self.bankStaticText = wx.StaticText(id=wxID_FRAMEBANKSTATICTEXT,
               label=u'Banque', name=u'bankStaticText',
-              parent=self.scrolledWindow, pos=wx.Point(1030, 38),
+              parent=self.scrolledWindow, pos=wx.Point(1030, 8),
               size=wx.Size(70, 20), style=wx.ALIGN_CENTRE)
         self.bankStaticText.SetHelpText(u'Cette entr\xe9e est-elle prise en compte par la banque ?')
 
         self.libelleStaticText = wx.StaticText(id=wxID_FRAMELIBELLESTATICTEXT,
               label=u'Libell\xe9', name=u'libelleStaticText',
-              parent=self.scrolledWindow, pos=wx.Point(230, 38),
+              parent=self.scrolledWindow, pos=wx.Point(230, 8),
               size=wx.Size(800, 20), style=wx.ALIGN_CENTRE)
 
         self.dateTextCtrl1 = wx.TextCtrl(id=wxID_FRAMEDATETEXTCTRL1,
               name=u'dateTextCtrl1', parent=self.scrolledWindow, pos=wx.Point(0,
-              62), size=wx.Size(99, 19), style=0, value=u'25/11/2010')
+              32), size=wx.Size(99, 19), style=0, value=u'25/11/2010')
 
         self.modeTextCtrl1 = wx.TextCtrl(id=wxID_FRAMEMODETEXTCTRL1,
               name=u'modeTextCtrl1', parent=self.scrolledWindow,
-              pos=wx.Point(100, 62), size=wx.Size(129, 19), style=0,
+              pos=wx.Point(100, 32), size=wx.Size(129, 19), style=0,
               value=u'CB Aur\xe9lie')
 
         self.libelleTextCtrl1 = wx.TextCtrl(id=wxID_FRAMELIBELLETEXTCTRL1,
               name=u'libelleTextCtrl1', parent=self.scrolledWindow,
-              pos=wx.Point(230, 62), size=wx.Size(799, 19), style=0,
+              pos=wx.Point(230, 32), size=wx.Size(799, 19), style=0,
               value=u'Chronodrive.com')
 
         self.bankCheckBox1 = wx.CheckBox(id=wxID_FRAMEBANKCHECKBOX1, label=u'',
               name=u'bankCheckBox1', parent=self.scrolledWindow,
-              pos=wx.Point(1048, 62), size=wx.Size(19, 19), style=0)
+              pos=wx.Point(1048, 32), size=wx.Size(19, 19), style=0)
         self.bankCheckBox1.SetValue(False)
 
         self.montantTextCtrl1 = wx.TextCtrl(id=wxID_FRAMEMONTANTTEXTCTRL1,
               name=u'montantTextCtrl1', parent=self.scrolledWindow,
-              pos=wx.Point(1100, 62), size=wx.Size(149, 19), style=wx.TE_RIGHT,
+              pos=wx.Point(1100, 32), size=wx.Size(149, 19), style=wx.TE_RIGHT,
               value=u'-83.56')
 
         self.selectCheckBox1 = wx.CheckBox(id=wxID_FRAMESELECTCHECKBOX1,
               label=u'', name=u'selectCheckBox1', parent=self.scrolledWindow,
-              pos=wx.Point(1264, 62), size=wx.Size(19, 19), style=0)
+              pos=wx.Point(1264, 32), size=wx.Size(19, 19), style=0)
         self.selectCheckBox1.SetValue(False)
 
         self.totalBanqueStaticText = wx.StaticText(id=wxID_FRAMETOTALBANQUESTATICTEXT,
               label=u'Total banque', name=u'totalBanqueStaticText',
-              parent=self.scrolledWindow, pos=wx.Point(900, 112),
+              parent=self.scrolledWindow, pos=wx.Point(900, 82),
               size=wx.Size(129, 19), style=0)
 
         self.totalStaticText = wx.StaticText(id=wxID_FRAMETOTALSTATICTEXT,
               label=u'Total', name=u'totalStaticText',
-              parent=self.scrolledWindow, pos=wx.Point(900, 92),
+              parent=self.scrolledWindow, pos=wx.Point(900, 62),
               size=wx.Size(129, 20), style=0)
 
         self.totalTextCtrl = wx.TextCtrl(id=wxID_FRAMETOTALTEXTCTRL,
               name=u'totalTextCtrl', parent=self.scrolledWindow,
-              pos=wx.Point(1100, 92), size=wx.Size(149, 19), style=wx.TE_RIGHT,
+              pos=wx.Point(1100, 62), size=wx.Size(149, 19), style=wx.TE_RIGHT,
               value=u'1000.05')
 
         self.totalBankTextCtrl = wx.TextCtrl(id=wxID_FRAMETOTALBANKTEXTCTRL,
               name=u'totalBankTextCtrl', parent=self.scrolledWindow,
-              pos=wx.Point(1100, 112), size=wx.Size(149, 19), style=wx.TE_RIGHT,
+              pos=wx.Point(1100, 82), size=wx.Size(149, 19), style=wx.TE_RIGHT,
               value=u'982.14')
 
     def __init__(self, parent):
