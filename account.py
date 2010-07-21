962e44bd6eed3698688166ff195131bc75ec96db
florent <florent@laas.fr>
2010-07-21 14:58:37 +0200

    Add a function to sort entries as on bank account
        * account.py.

--- account.py
+++ account.py
@@ -4,6 +4,38 @@ import re
 from entry import *
 import codecs
 
+
+def bankOrder(e1, e2):
+    """
+    Sort entries as on bank statement
+    """
+    modeOrder = {u'CB Florent':0, u'e-CB':0, u'CB Aurélie':1}
+    if e1.bank and e2.bank:
+        return 0
+
+    if not e1.bank and e2.bank:
+        return 1
+
+    if e1.bank and not e2.bank:
+        return -1
+
+    # if both entries are not on previous statement, sort by mode then by date
+    # First add cheques in modeOrder map
+    m1 = e1.mode
+    m2 = e2.mode
+    for m in (m1, m2):
+        if not m in modeOrder :
+            if m[:3] == 'CHQ':
+                modeOrder[m] = 2
+            else :
+                modeOrder[m] = 3
+
+    if modeOrder[m1] != modeOrder[m2]:
+        return cmp(modeOrder[m1], modeOrder[m2])
+
+    # Sort by date
+    return cmp(e1.date, e2.date)
+
 class Account (object) :
 
     def __init__(self):
@@ -70,11 +102,11 @@ class Account (object) :
             f.write( 3*"\t"+"balance: "+str(self.balance))
             f.write( "\t"+"bank: "+str(self.bank_balance))
 
-    def sort (self) :
+    def sort (self, comp = lambda x,y: cmp(x.date,y.date)) :
         """
         Sort entries by increasing dates
         """
-        self.entries.sort(lambda x,y: cmp(x.date,y.date))
+        self.entries.sort(comp)
 
     def __str__(self) :
         output = ""
